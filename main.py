fc2063ba9c23e13201917b2ee7dbe5f6d8de061e
keawi26 <keawi26@365.lexca.org>
2025-12-01 10:48:27 -0500

    Added extremely rough framework for zone temperatures.
    Took 1 hour 7 minutes

--- main.py
+++ main.py
@@ -42,9 +42,59 @@ with gui.window(label = "Reactor Data", tag = "reactor_data"): #TODO: come up wi
     gui.add_text("add the stuff on the computer about water temp, pressure, etc.")
     #TODO: add data labels
 
-with gui.window(label = "Zone Temperatures", tag = "zone_temperatures"):
-    gui.add_text("add zone temperatures for balancing reactor temps")
-    #TODO: add rectangles for zones
+zone_colors = draw.lerp_rgb((0, 0, 1), (1, 0, 0), 5001)
+zones = config["zones"]
+
+with gui.window(label = "Zone Temperatures", tag = "zone_temperatures"): #FIXME: ugly code
+
+    r_a2, g_a2, b_a2 = zone_colors[zones[0][0]]
+
+    r_a3, g_a3, b_a3 = zone_colors[zones[0][1]]
+
+    r_d2, g_d2, b_d2 = zone_colors[zones[3][0]]
+
+    r_d3, g_d3, b_d3 = zone_colors[zones[3][1]]
+
+    draw.draw_reactor_zone(
+        gui,
+        10.0 + 90.0, 10.0,
+        fill = (255 * r_a2, 255 * g_a2, 100 * b_a2),
+        text = zones[0][0]
+    )
+
+    draw.draw_reactor_zone(
+        gui,
+        (10.0 + 90.0) * 2, 10.0,
+        fill = (255 * r_a3, 255 * g_a3, 255 * b_a3),
+        text = zones[0][1]
+    )
+
+    for i in range(1, 3):
+        for j in range (0, 4):
+
+            r, g, b = zone_colors[zones[i][j]]
+
+            draw.draw_reactor_zone(
+                gui,
+                (10.0 + 90.0) * j, 10.0 + 60.0 * i,
+                fill = (255 * r, 255 * g, 255 * b),
+                text = zones[i][j]
+            )
+
+    draw.draw_reactor_zone(
+        gui,
+        10.0 + 90.0, 10.0 + 60.0 * 3,
+        fill = (255 * r_d2, 255 * g_d2, 255 * b_d2),
+        text = zones[3][0]
+    )
+
+    draw.draw_reactor_zone(
+        gui,
+        (10.0 + 90.0) * 2, 10.0 + 60.0 * 3,
+        fill = (255 * r_d3, 255 * g_d3, 255 * b_d3),
+        text = zones[3][1]
+    )
+
 
 gui.create_viewport(title = "Reactor", width = 800, height = 600)
 
